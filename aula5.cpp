196c36683f5f3a33e8d58d575359f02f9808f46e
RenatoRufus <renatooffice1@gmail.com>
2023-10-29 19:39:26 -0300

    .

---
+++ aula5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define canal cout<<"Canal fessor bruno\n\n";
+
+int main(){
+
+    int vidas = 3;
+    int tiros = 500;
+    int life = 10;
+
+    int zero, um, dois;
+
+    cout<< pi <<"\n";
+    canal
+
+
+
+
+    return 0;
+}

2275c1e8832dcec74f2365c6060bb097af897713
RenatoRufus <renatooffice1@gmail.com>
2023-11-09 21:23:47 -0300

    .

---
+++ aula16.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //do while
+
+    int cont;
+
+    cont=20;
+    do
+    {
+        cout<<"Texto - "<< cont <<"\n";
+        cont++;
+    } while (cont < 20);
+           
+    
+
+    cout<<"Rotina finalizada\n";
+
+
+    return 0;
+}

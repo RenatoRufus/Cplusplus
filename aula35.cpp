9023ad30deb79bb491a106d01c81d6f3d7819337
RenatoRufus <renatooffice1@gmail.com>
2023-12-09 21:54:37 -0300

    .

--- aula35.cpp
+++ aula35.cpp
@@ -44,12 +44,27 @@ struct Carro
 
 int main(){
     //struct
-    Carro car;
+    Carro *carros = new Carro[5];
+    Carro car1,car2,car3,car4,car5;
+
+
+    carros[0] = car1;
+    carros[1] = car2;
+    carros[2] = car3;
+    carros[3] = car4;
+    carros[4] = car5;
+
+    carros[0].insere("Tornado","Vermelho", 450,350);
+    carros[1].insere("Luxo","Preto", 250,260);
+    carros[2].insere("Família","Prata", 150,180);
+    carros[3].insere("Trabalho","Branco", 80,120);
+    carros[4].insere("Padrão","Cinza", 100,150);
     
-    car.insere("No name", "Vermelho", 450, 350);
-    car.mostra();
-    car.mudaVel(1150);
-    car.mostra();
+    for (int i = 0; i < 5; i++)
+    {
+        carros[i].mostra();
+    }
+    
 
     return 0;
 } 

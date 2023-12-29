3f5bc2719cc36459453516302295c55b1e74835f
RenatoRufus <renatooffice1@gmail.com>
2023-12-29 18:16:59 -0300

    .

---
+++ 43aula.cpp
@@ -0,0 +1,17 @@
+//system
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    system("ls");
+    system("cd /home/jr");
+    cout<<"Limpo\n";
+    system("gedit");
+
+    
+
+    return 0;
+}

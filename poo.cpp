eec2e179b161643a25f7423a123b594fd9edb60b
RenatoRufus <renatooffice1@gmail.com>
2024-01-09 20:09:49 -0300

    .

--- poo.cpp
+++ poo.cpp
@@ -2,9 +2,45 @@
 
 using namespace std;
 
+class aviao{
+
+public:
+    int velocidade=0;
+    int velMax;
+    string tipo;
+    void ini (int tp); //método
+
+private:
+
+
+};
+
+void aviao::ini(int tp){
+    if (tp == 1)
+    {
+        this->velMax=800;
+        this->tipo="Jato";
+    }else if(tp == 2){
+        this->velMax=350;
+        this->tipo="Monomotor";
+    }else if(tp == 3){
+        this->velMax=180;
+        this->tipo="Planador";
+    }
+    
+
+}
+
 int main(){
 
-    
+    aviao *av1 = new aviao();
+    aviao *av2 = new aviao();
+
+    av1->ini(3);
+    av2->ini(1);
+
+    cout<< av1->velMax <<endl;
+    cout<< av2->velMax;
 
     return 0;
 }
